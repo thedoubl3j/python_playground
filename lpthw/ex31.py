38e475c0f12ee21dd8a67b02d4c1bfa887d899c9
Jake Jackson <jljacks93@gmail.com>
2019-04-16 16:32:30 -0400

    another example and then the start of the warcraft game

---
+++ lpthw/ex31.py
@@ -0,0 +1,37 @@
+print("""You enter a dark room with 2 doors.
+Do you go through door #1 or door #2?""")
+
+door = input("> ")
+
+if door == "1":
+    print("There's a giant bear here eating a cheese cake.")
+    print("What do you do?")
+    print("1. take the cake.")
+    print("2. scream at the bear")
+
+    bear = input("> ")
+
+    if bear == "1":
+        print("The bear eats your faceoff. Good Job!")
+    elif bear == "2":
+        print("The bear eats your legs off. Good Job!")
+    else :
+        print(f"Well, doing {bear} is probably better.")
+        print("Bear runs away.")
+
+elif door == "2":
+    print("You stare into the endless abyss in Cthulu's retina.")
+    print("1. blueberries")
+    print("2. yellow jacket clothespins")
+    print("3. understanding revolvers yelling melodies")
+
+    insanity = input ("> ")
+    if insanity == "1" or insanity == "2":
+        print("Your body survives powered by a mind of Jello")
+        print("Good Job!")
+    else:
+        print("The insanity rots your eyes into a pool of muck")
+        print("Good Job!")
+
+else:
+    print("You stumble around anf all on a knife and die.")
---
+++ lpthw/own_game.py
@@ -0,0 +1,20 @@
+#wip
+print("""The Warcraft faction game.
+Choose your faction.""")
+print("1. Horde")
+print("2. Alliance")
+#select the factions
+faction = input("> ")
+#horde logic tree, finish out the "no orc" idea
+if faction == "1":
+    print("Lok'tar ogar friend. For the Horde!")
+    print("What race do you play?")
+    print("1. Orc")
+    print("2. Not orc")
+
+    horde_race = ("> ")
+    if horde_race == "1":
+
+
+
+#allianca logic tree. something punny about the boy king could work and also maybe dive one more layer deeper or add something else. 

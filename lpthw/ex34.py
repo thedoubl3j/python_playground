71c0d180632ca3f91d40511c78f636a62ae41373
Jake Jackson <jljacks93@gmail.com>
2019-04-30 15:19:13 -0400

    list work

---
+++ lpthw/ex34.py
@@ -0,0 +1,19 @@
+#list practice. helping learn cardinal vs ordinal lists
+animals = ['bear', 'python3.6', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+#the animal at 1
+animals[1] = python
+#the third animal
+animals[2] = peacock
+#the first animal
+animals[0] = Bear
+#the animal at 3
+animals[3] = kangaroo
+#the fifth animal
+animals[4] = whale
+#the animal at 2
+print(peacock)
+#the sixth animal
+animals[5] = platypus
+#the animal at 4
+print(whale)

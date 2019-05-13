af52986dfe3789fffd989aacba282cc3ad14de93
Jake Jackson <jljacks93@gmail.com>
2019-05-12 23:32:23 -0400

    dict work, not done yet

---
+++ lpthw/ex39.py
@@ -0,0 +1,14 @@
+# create a mapping of state to abbrevatio
+states = {
+    'Oregon': 'OR',
+    'Florida': 'FL',
+    'California': 'CA',
+    'New York': 'NY',
+    'Michigan': 'MI'
+}
+
+#create a basic set of states and some cities in them
+cities = {
+    'CA': 'San Francisco',
+    
+}

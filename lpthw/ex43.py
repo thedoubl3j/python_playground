4ed5934bbbf2efcf4bdf2c14d5badcde5a7fa11c
Jake Jackson <jljacks93@gmail.com>
2019-07-16 23:17:00 -0400

    adding options for engagement

--- lpthw/ex43.py
+++ lpthw/ex43.py
@@ -40,11 +40,44 @@ class Death(Scene):
     def enter(self):
         print(Death.quips[randint90, len(self.quips)-1])
         exit(1)
-
 class CentralCorridor(Scene):
 
     def enter(self):
-        pass
+        print(dedent("""
+        The Gothons of the Planer Percal number 25 have invaded your ship and destroyed your entire crew. You are the last surviving member and your last missing is to get to the neutron destruct
+        bomb from the Weapons Armory, put it in the bride and blow the ship up after getting into an escape pod.
+
+        You're running down the central corridor to the weapons armory when a Gothon jumps out, red scaly skin, dark grimy teeth and evil clown costume flowing around his hate
+        filled body. He is blocking the door to the armory and about to pull a weapon to blast you.
+        """))
+
+        action = input("> ")
+        if action == "shoot!":
+            print(dedent("""
+                  Quick on the draw, you yank out ypour blaster and fire at the Gothon. His clown costume is flowing and moving around his body which throws off your aim.
+                  Your laser hits his costume but misses him entirely. Thie completely ruins his costume that his mother bought him,. Which enrages him even more and causes him to blast you
+                  in the face until you are dead. After that, he consumes your withering soul.
+            """))
+            return 'death'
+
+        elif action == "Dodge!":
+            print(dedent("""
+                Like a world class boxzer, you dodge weave, slip and slide past shots coming your way. ONly in the middle of the moves do you slip and crack your head on the floor then pass out.
+                You wake up only to get crushed by the gothon and then eaten.
+                """))
+                return 'death'
+
+        elif action == "tell a joke":
+            print(dedent("""
+                  Luckily for you, they made you learn Gothon insdules in the academy. You the one gothon joke you know: existence is pain, wuba luba dub dub.
+                  The gothon stops, tries not to laugh then breaks out cry laughing, startling you because that noise reminds you of a distant past.
+                  Anywho, you go up, shoot him in the head a point blank range and get to the missle room.
+                  """))
+                return 'laser_weapon_armory'
+
+        else:
+            print("Incorrect, please try again useless human.")
+            return 'cental_corridor'
 
 class LaserWeaponArmory(Scene):
 

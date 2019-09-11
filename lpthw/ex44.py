d3ca11c0c2815658fbe3ed05288fc196c27fc055
Jake Jackson <jljacks93@gmail.com>
2019-09-11 15:56:55 -0400

    need to get to altered/super next

--- lpthw/ex44.py
+++ lpthw/ex44.py
@@ -1,13 +1,14 @@
 class Parent(object):
 
-    def implicit(self):
-        print("PARENT implicit()")
+    def override(self):
+        print("PARENT override()")
 
 class Child(Parent):
-    pass
+    def override(self):
+        print("CHILD override()")
 
 dad = Parent()
 son = Child()
 
-dad.implicit()
-son.implicit()
+dad.override()
+son.override()

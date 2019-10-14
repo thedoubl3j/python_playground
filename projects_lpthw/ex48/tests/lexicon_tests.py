cbf931fd50f2550948eaa43b1b88874142d41edf
Jake Jackson <jljacks@gmail.com>
2019-10-13 21:49:13 -0400

    late but pushing up last weeks work

--- projects_lpthw/ex48/tests/lexicon_tests.py
+++ projects_lpthw/ex48/tests/lexicon_tests.py
@@ -18,3 +18,25 @@ def test_verbs():
 def test_stops():
     assert_equal(lexicon.scan("the"), [('stop', 'the')])
     result = lexicon.scan("the in of")
+    assert_equal(result, [('stop', 'the'),
+                          ('stop', 'in'),
+                          ('stop', 'of')])
+
+def test_nouns():
+    assert_equal(lexicon.scan("bear"), [('noun', 'bear')])
+    result = lexicon.scan("bear princess")
+    assert_equal(result, [('noun', 'bear'),
+                          ('noun', 'princess')])
+
+def test_numbers():
+    assert_equal(lexicon.scan("1234"), [('number', 1234)])
+    result = lexicon.scan("3 91234")
+    assert_equal(result, [('noun', 'bear'),
+                          ('noun', 'princess')])
+
+def test_errors():
+    assert_equal(lexicon.scan("ASDFASDF"), [('error', 'ASDFASDF')])
+    result = lexicon.scan("bear IAS princess")
+    assert_equal(result, [('noun', 'bear'),
+                          ('error', 'IAS')
+                          ('noun', 'princess')])    
